30588e5bc694263ec1ca97ad58cb7d459007c817
pxy <1611474513@qq.com>
2022-04-19 17:23:56 +0800

    完成了首页功能

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 import Vue from 'vue'
 import App from './App'
 
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+// $http.baseurl = 'https://www.uinav.com'
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title:'数据加载中...'
+  })
+}
+
+//封装弹框失败
+uni.$showMsg = function(title="请求数据失败！",duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
